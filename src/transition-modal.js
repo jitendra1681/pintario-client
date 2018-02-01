8e8adff5b4f762e187068ba78269eed32872ff98
ashish <ashishchoudharylinkites@gmail.com>
2018-02-01 20:01:06 +0530

    onclose last

--- src/transition-modal.js
+++ src/transition-modal.js
@@ -6,46 +6,13 @@ import './App.css'
 import ReactTransitions from 'react-transitions'
 import 'react-transitions/dist/animations.css'
 
-const centeredStyle = {
-  position: 'absolute',
-  transform: 'translate(-50%, -50%)',
-  top: '50%',
-  left: '50%'
-};
-const noWrapStyle = {
-  whiteSpace: 'nowrap'
-};
 const childStyle = {
   width: '100%',
   height: '100%',
   backgroundColor: 'powderblue'
-};
-const divGrandStyle = {
-  textAlign: 'center',
-  fontFamily: '"Lato", Calibri, Arial, sans-serif',
-  fontSize: 60,
-  color: '#fff'
-};
-const divCenteredGrandStyle = {
-  ...divGrandStyle,
-  ...centeredStyle
-};
-const divSemiGrandStyle = {
-  ...divGrandStyle,
-  height: '50%',
-  position: 'relative'
-};
-const divSemiGrandUpStyle = {
-  ...divSemiGrandStyle,
-  backgroundColor: 'lightpink'
-};
-const divSemiGrandDownStyle = {
-  ...divSemiGrandStyle,
-  backgroundColor: 'lightgreen',
-  ...noWrapStyle
-};
+}
 
-class App extends Component { 
+class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
@@ -57,9 +24,10 @@ class App extends Component {
   componentWillMount() {
     const { images } = this.props
     const array = []
+    images[images.length-1].last = 'last'
     images.map((image) => {
       if (image.ContentType !== 4) {
-        array.push(<img src={image.URL} style={ childStyle } alt={image.sessionTransition}/>)
+        array.push(<img src={image.URL} style={ childStyle } alt={image.sessionTransition} alt2={image.last}/>)
       } else {
         array.push(<video controls autoPlay>
           <source src={image.URL} type="video/mp4"/>
@@ -68,7 +36,13 @@ class App extends Component {
     })
     this.setState({ images: array })
   }
-
+  onTransitionClick(last) {
+    if (last === 'last') {
+      this.props.closeModal()
+    } else {
+      this.setState({ clicked: this.state.clicked + 1 })
+    }
+  }
   render() {
     const { clicked, images } = this.state
     const index = this.state.clicked % images.length
@@ -79,7 +53,7 @@ class App extends Component {
           transition={comp.props.alt === 'rotate' ? 'rotate-cube-left-out-rotate-cube-left-in' : 'move-to-left-move-from-right'}
           width={ 600 }
           height={ 300 }
-          onClick={ () => this.setState( { clicked: this.state.clicked + 1 } ) }
+          onClick={this.onTransitionClick.bind(this, comp.props.alt2)}
         >
         { comp }
         </ReactTransitions>
